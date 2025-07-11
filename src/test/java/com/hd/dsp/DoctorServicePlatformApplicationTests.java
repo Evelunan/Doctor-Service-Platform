6571c0c5d25be5360fb7e54361a949ec32900cb6
ykk <2804325670@qq.com>
2025-07-11 15:03:09 +0800

    项目初始化

---
+++ src/test/java/com/hd/dsp/DoctorServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.dsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServicePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
